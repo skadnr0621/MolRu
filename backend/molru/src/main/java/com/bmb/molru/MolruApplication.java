d63a418f36cacb52902d40d8014a715e083ca92a
zlflshdh84 <zlflshdh84@gmail.com>
2022-03-25 14:31:35 +0900

    Feat : init backend

---
+++ backend/molru/src/main/java/com/bmb/molru/MolruApplication.java
@@ -0,0 +1,13 @@
+package com.bmb.molru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolruApplication.class, args);
+	}
+
+}
