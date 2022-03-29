a4d2195c6169f5937ee9db353bd115f5ca9c58fa
zlflshdh84 <zlflshdh84@gmail.com>
2022-03-29 11:38:06 +0900

    Feat : add Sale MVC prototype structure

--- backend/molru/src/main/java/com/bmb/molru/controller/UserController.java
+++ backend/molru/src/main/java/com/bmb/molru/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/user")
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
