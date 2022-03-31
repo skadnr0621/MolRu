61bc286cc8488cc621a71684fe2ab5191a1cd86f
im-cherry <collcr@kakao.com>
2022-03-31 18:11:32 +0900

    Chore: babel/runtime & mui/x-data-grid 설치

--- frontend/src/components/ItemDetailActivity.js
+++ frontend/src/components/ItemDetailActivity.js
@@ -1,66 +1,80 @@
 import React from 'react'
 import Box from '@mui/material/Box'
-// import { DataGrid } from '@mui/x-data-grid'
+import { DataGrid, getGridStringOperators } from '@mui/x-data-grid'
 
-// const columns = [
-//   { field: 'id', headerName: 'ID', width: 90 },
-//   {
-//     field: 'firstName',
-//     headerName: 'First name',
-//     width: 150,
-//     editable: true,
-//   },
-//   {
-//     field: 'lastName',
-//     headerName: 'Last name',
-//     width: 150,
-//     editable: true,
-//   },
-//   {
-//     field: 'age',
-//     headerName: 'Age',
-//     type: 'number',
-//     width: 110,
-//     editable: true,
-//   },
-//   {
-//     field: 'fullName',
-//     headerName: 'Full name',
-//     description: 'This column has a value getter and is not sortable.',
-//     sortable: false,
-//     width: 160,
-//     valueGetter: (params) =>
-//       `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-//   },
-// ]
+function loadServerRows(commodityFilterValue) {
+  const serverRows = [
+    { id: '1', commodity: 'rice' },
+    { id: '2', commodity: 'soybeans' },
+    { id: '3', commodity: 'milk' },
+    { id: '4', commodity: 'wheat' },
+    { id: '5', commodity: 'oats' },
+  ]
 
-// const rows = [
-//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-// ]
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      if (!commodityFilterValue) {
+        resolve(serverRows)
+        return
+      }
+      resolve(
+        serverRows.filter((row) =>
+          row.commodity.toLowerCase().includes(commodityFilterValue),
+        ),
+      )
+    }, Math.random() * 500 + 100) // simulate network latency
+  })
+}
+
+const columns = [
+  {
+    field: 'commodity',
+    width: 150,
+    filterOperators: getGridStringOperators().filter(
+      (operator) => operator.value === 'contains',
+    ),
+  },
+]
 
 const ItemDetailActivity = () => {
+  const [rows, setRows] = React.useState([])
+  const [filterValue, setFilterValue] = React.useState()
+  const [loading, setLoading] = React.useState(false)
+
+  const onFilterChange = React.useCallback((filterModel) => {
+    setFilterValue(filterModel.items[0].value)
+  }, [])
+
+  React.useEffect(() => {
+    let active = true
+
+    ;(async () => {
+      setLoading(true)
+      const newRows = await loadServerRows(filterValue)
+
+      if (!active) {
+        return
+      }
+
+      setRows(newRows)
+      setLoading(false)
+    })()
+
+    return () => {
+      active = false
+    }
+  }, [filterValue])
+
   return (
-    <Box>
-      아이템 활동
-      {/* <div style={{ height: 400, width: '100%' }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-          checkboxSelection
-          disableSelectionOnClick
-        />
-      </div> */}
-    </Box>
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        filterMode="server"
+        onFilterModelChange={onFilterChange}
+        loading={loading}
+      />
+    </div>
   )
 }
 
