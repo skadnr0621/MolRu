f87bd29d2ad8c96ec92cf849be8ee3bc6982b253
오용록 <ohyr9685@gmail.com>
2022-04-06 04:10:44 +0900

    Feat: add user info at my page

--- frontend/src/components/AccountInfo.js
+++ frontend/src/components/AccountInfo.js
@@ -1,38 +1,71 @@
-import Avatar from '@mui/material/Avatar'
+import { useState, useEffect, useContext } from 'react'
+import { AppContext } from 'contexts/context'
+import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 
 import AccountTabs from './AccountTabs'
+import MelodyBackground from './MelodyBackground'
 
 const AccountInfo = () => {
+  const { state, actions } = useContext(AppContext)
+
   return (
     <Container
       maxWidth="lg"
       sx={{
-        mt: 3,
+        mt: 5,
         mb: 14,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
       }}
     >
-      <Avatar
+      <MelodyBackground />
+      <Box
         sx={{
           m: 2,
-          width: 100,
-          height: 100,
-          backgroundColor: 'black',
-          fontSize: 40,
+          width: '150px',
+          height: '150px',
+          borderRadius: '50%',
+          border: '2px solid rgb(229, 232, 235)',
+          display: 'flex',
+          overflow: 'hidden',
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
-        M
-      </Avatar>
-      <Typography variant="h5" component="div" gutterBottom>
-        김남욱
+        <img src={state.imageUrl} style={{ width: '100%', height: 'auto' }} />
+        {/* </label> */}
+      </Box>
+      <Typography
+        sx={{
+          m: 1,
+          fontWeight: 'bold',
+          fontSize: '30px',
+          color: 'rgba(0, 0, 0, 0.87)',
+        }}
+      >
+        {state.nickname}
       </Typography>
-      <Typography variant="subtitle1" component="div" gutterBottom>
-        0x34df...2312
-      </Typography>
+      <Box
+        sx={{
+          mb: 3,
+          border: '1px solid #707A83',
+          borderRadius: '20px',
+          padding: '4px 8px',
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: 'bold',
+            fontSize: '16px',
+            color: '#707A83',
+          }}
+        >
+          {state.account}
+        </Typography>
+      </Box>
       <AccountTabs />
     </Container>
   )
