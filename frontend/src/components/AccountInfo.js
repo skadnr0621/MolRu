d7fffc64072c3f8cbffd0325ffac9d0d868e518d
im-cherry <collcr@kakao.com>
2022-04-07 01:48:27 +0900

    Design: 프로필 페이지 반응형 구현

--- frontend/src/components/AccountInfo.js
+++ frontend/src/components/AccountInfo.js
@@ -2,10 +2,14 @@ import { useState, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { AppContext } from 'contexts/context'
 import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
-
+import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import AccountTabs from './AccountTabs'
 import MelodyBackground from './MelodyBackground'
 
@@ -15,6 +19,19 @@ const AccountInfo = () => {
   const onLogout = () => {
     alert('로그아웃')
     actions.handleConnect()
+  }
+
+  const onCopyAccount = () => {
+    var copyText = document.getElementById('wallet-address').value
+
+    navigator.clipboard
+      .writeText(copyText)
+      .then(() => {
+        alert(`복사 성공!`)
+      })
+      .catch(() => {
+        alert(`복사 실패!`)
+      })
   }
 
   return (
@@ -40,6 +57,14 @@ const AccountInfo = () => {
           overflow: 'hidden',
           alignItems: 'center',
           justifyContent: 'center',
+          '@media(max-width: 600px)': {
+            width: '100px',
+            height: '100px',
+          },
+          '@media(max-width: 480px)': {
+            width: '80px',
+            height: '80px',
+          },
         }}
       >
         <img src={state.imageUrl} style={{ width: '100%', height: 'auto' }} />
@@ -47,41 +72,56 @@ const AccountInfo = () => {
       </Box>
       <Typography
         sx={{
-          m: 1,
+          // m: 1,
           fontWeight: 'bold',
           fontSize: '30px',
           color: 'rgba(0, 0, 0, 0.87)',
+          '@media(max-width: 600px)': {
+            fontSize: '24px',
+          },
+          '@media(max-width: 480px)': {
+            fontSize: '20px',
+          },
         }}
       >
         {/* {state.nickname} */}
         hello
       </Typography>
-      <Box
-        sx={{
-          mb: 2,
-          border: '1px solid #707A83',
-          borderRadius: '20px',
-          padding: '4px 8px',
-        }}
-      >
-        <Typography
+      <Box sx={{ m: 2 }}>
+        <TextField
           sx={{
-            fontWeight: 'bold',
-            fontSize: '16px',
-            color: '#707A83',
+            backgroundColor: 'white',
+            color: 'rgba(0, 0, 0, 0.87)',
+            '& .MuiOutlinedInput-root': {
+              paddingRight: '0px',
+            },
           }}
-        >
-          {state.account}
-        </Typography>
+          id="wallet-address"
+          size="small"
+          variant="outlined"
+          value={state.account}
+          InputProps={{
+            disabled: true,
+            endAdornment: (
+              <InputAdornment position="start">
+                <IconButton onClick={onCopyAccount}>
+                  <ContentCopyIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          fullWidth
+        />
       </Box>
-      <Box sx={{ mb: 3 }}>
+
+      <Stack direction="row" spacing={5}>
         <Button
           variant="contained"
           sx={{ textTransform: 'none' }}
           to="/account/setting"
           component={Link}
         >
-          <Typography sx={{ fontWeight: 'bold' }}>profile edit</Typography>
+          <Typography sx={{ fontWeight: 'bold' }}>edit</Typography>
         </Button>
         <Button
           variant="outlined"
@@ -95,7 +135,8 @@ const AccountInfo = () => {
             logout
           </Typography>
         </Button>
-      </Box>
+      </Stack>
+
       <AccountTabs />
     </Container>
   )
