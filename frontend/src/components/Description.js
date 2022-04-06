7dca588024e69bce2631e7687ae73091de5d1791
오용록 <ohyr9685@gmail.com>
2022-04-06 18:23:58 +0900

    Design: update icons

--- frontend/src/components/Description.js
+++ frontend/src/components/Description.js
@@ -5,21 +5,26 @@ import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined'
+import MusicNoteOutlinedIcon from '@mui/icons-material/MusicNoteOutlined'
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined'
 
 const Description = () => {
+  const iconStyle = { fontSize: '40px', color: 'rgba(0, 0, 0, 0.87)' }
+
   const descriptions = [
     {
-      icon: `<AccountBalanceWalletOutlinedIcon />`,
+      icon: <AccountBalanceWalletOutlinedIcon sx={iconStyle} />,
       title: `지갑 설정`,
       content: `지갑을 생성한 후,<br />오른쪽 상단 모서리에 있는 지갑 아이콘을<br />클릭하여 MOLRU에 연결하세요.`,
     },
     {
-      icon: `<AccountBalanceWalletOutlinedIcon />`,
+      icon: <MusicNoteOutlinedIcon sx={iconStyle} />,
       title: `NFT 생성`,
       content: `Create 를 클릭하여<br />멜로디 NFT 뽑기를 합니다.<br />이때, 멜로디는 무작위로 생성됩니다.`,
     },
     {
-      icon: `<AccountBalanceWalletOutlinedIcon />`,
+      icon: <StorefrontOutlinedIcon sx={iconStyle} />,
       title: `NFT 판매`,
       content: `마이 페이지 > NFT 목록 > 소유한 NFT 에서<br />원하는 NFT를 판매할 수 있습니다.`,
     },
