44dccf4870807d93b4b66181b46d29a2b7f3d4c0
im-cherry <collcr@kakao.com>
2022-04-08 00:33:51 +0900

    Feat: 검색 기능 구현

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,23 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { makeStyles, styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import Stack from '@mui/material/Stack'
 import Autocomplete from '@mui/material/Autocomplete'
 
-const SearchBar = ({ top100Films }) => {
-  const SearchBarStyle = styled('div')({
-    display: 'flex',
-    height: '72px',
-    width: '100%',
-    maxWidth: '768px',
-    alignItems: 'center',
-    margin: '0px 16px',
+const SearchBarStyle = styled('div')({
+  display: 'flex',
+  height: '72px',
+  width: '100%',
+  maxWidth: '768px',
+  alignItems: 'center',
+  margin: '0px 16px',
 
-    '@media(max-width: 480px)': {
-      display: 'none',
-    },
-  })
+  '@media(max-width: 480px)': {
+    display: 'none',
+  },
+})
+
+const SearchBar = ({ top100Films }) => {
+  const navigate = useNavigate()
+
+  const [searchContent, setSearchContent] = useState('')
+
+  const onEnterKey = (event) => {
+    if (event.key == 'Enter') {
+      navigate(`/items?category=전체&status=all&search=${searchContent}`)
+    }
+  }
 
   return (
     <SearchBarStyle id="search-bar">
@@ -25,6 +36,9 @@ const SearchBar = ({ top100Films }) => {
         sx={{ width: 'inherit' }}
         id="free-solo-demo"
         freeSolo
+        value={searchContent}
+        onChange={() => setSearchContent(event.target.value)}
+        onKeyPress={onEnterKey}
         options={top100Films.map((option) => option.title)}
         renderInput={(params) => <TextField {...params} label="freeSolo" />}
       />
